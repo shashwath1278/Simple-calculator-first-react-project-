bc8f852a33c05ad1bda2cd919fb06591611fda5a
shashwath1278 <drs.prabhu2018@gmail.com>
2024-09-03 08:44:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css'; 
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm"; 
+
+function App() {
+  return (
+<>
+
+      <Navbar/>
+     
+      
+      <div className="container">
+  <div className="text-block"><p>
+    <center><h3> SIMPLE&nbsp;CALCULATOR </h3></center><br/>
+      BASIC JS ARITHMETIC OPERATORS<br /><br />
+
+    Operator&nbsp;&nbsp;&nbsp;&nbsp;Description<br /><br />
+
+      +&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Addition<br />
+      -&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Subtraction<br />
+      *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Multiplication<br />
+      **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentiation(ES2016)<br />
+      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Division<br />
+      %&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Modulus (Remainder)
+    </p>
+  </div>
+
+  <div className="container"><TextForm/></div>
+</div>
+
+
+      </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+
+export default function TextForm() {
+
+    const Upclick = ()=>{
+       setText(eval(text));
+    }
+
+    const Downclick = ()=>{
+      setText("");
+    }
+    
+    const onChange= (event)=>{
+        setText(event.target.value);
+    }
+    const [text, setText] = useState(' ');
+  return (
+    <div>
+ <div className="mb-3">
+  <label for="Mytext" className="form-label">Enter your arithmetic problem</label>
+  <textarea className="form-control" value={text} onChange={onChange} id="Mytext" rows="6" columns="10"></textarea>
+</div>
+<button className="btn btn-primary" onClick={Upclick} >enter</button>
+<button className="btn btn-primary" onClick={Downclick} >back</button>
+
+</div>
+
+
+
+  )
+}
+
