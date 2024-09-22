b6885b9c45aa7fdf08ad965f00e261dedba0b953
shash <drs.prabhu2018@gmail.com>
2024-09-22 09:37:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,16 +11,19 @@ function App() {
       
       <div className="container">
   <div className="text-block"><p>
-    <center><h3> SIMPLE&nbsp;CALCULATOR </h3></center><br/>
-      BASIC JS ARITHMETIC OPERATORS<br /><br />
+    <center><h3> SIMPLE&nbsp;CALCULATOR </h3></center>
+    <br/>
+      BASIC JS ARITHMETIC OPERATORS
+    <br />
+    <br />
 
     Operator&nbsp;&nbsp;&nbsp;&nbsp;Description<br /><br />
 
-      +&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Addition<br />
-      -&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Subtraction<br />
-      *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Multiplication<br />
-      **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentiation(ES2016)<br />
-      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Division<br />
+      +&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Addition<br/>
+      -&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Subtraction<br/>
+      *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Multiplication<br/>
+      **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exponentiation(ES2016)<br/>
+      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Division<br/>
       %&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Modulus (Remainder)
     </p>
   </div>
